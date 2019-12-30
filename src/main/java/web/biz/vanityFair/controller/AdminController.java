026b9542b60682a04875b6029d3ebbb10ebf40ad
InSeong-So <goflvhxj2547@gmail.com>
2019-12-30 14:59:20 +0900

    기본 틀 구성 완료

--- src/main/java/web/biz/vanityFair/controller/AdminController.java
+++ src/main/java/web/biz/vanityFair/controller/AdminController.java
@@ -1,13 +1,14 @@
 package web.biz.vanityFair.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
 import web.common.core.component.SisExtends;
 
+@Slf4j
 @Controller
 @RequestMapping("/admin")
 public class AdminController extends SisExtends {
@@ -17,19 +18,46 @@ public class AdminController extends SisExtends {
     @GetMapping
     public ModelAndView admin_total() {
         ModelAndView view = new ModelAndView();
-        
+
         view.setViewName(menuName);
         view.addObject("leftMenu", "status");
-        
+
         return view;
     }
-    
+
     @GetMapping("/{adminNumber}/{adminSubNumber}")
     public ModelAndView admin_left(@PathVariable("adminNumber") String adminNumber, @PathVariable("adminSubNumber") String adminSubNumber) {
         ModelAndView view = new ModelAndView();
 
+        switch (adminNumber) {
+            case "01":
+                log.info("상품분류관리 메뉴");
+                break;
+            case "02":
+                log.info("상품옵션관리 메뉴");
+                break;
+            case "03":
+                log.info("상품등록 메뉴");
+                break;
+            case "04":
+                log.info("상품관리 메뉴");
+                break;
+            case "05":
+                log.info("상품표시관리 메뉴");
+                break;
+            case "06":
+                log.info("판매분류관리 메뉴");
+                break;
+            case "07":
+                log.info("재고관리 메뉴");
+                break;
+            default:
+                break;
+        }
+
         view.setViewName(menuName);
         view.addObject("leftMenu", adminNumber);
+        view.addObject("subMenu", adminSubNumber);
 
         return view;
     }
--- src/main/java/web/biz/vanityFair/domain/product/Product.java
+++ src/main/java/web/biz/vanityFair/domain/product/Product.java
@@ -33,194 +33,194 @@ public class Product {
     private long productNo;
 
     // 상품명
-    @Column
+    @Column(columnDefinition = "varchar(100)")
     private String productNm;
 
     // 상품분류
-    @Column
+    @Column(columnDefinition = "varchar(20)")
     private String productCategory;
 
     // 원산지
-    @Column
+    @Column(columnDefinition = "varchar(20)")
     private String origin;
 
     // 제조사
-    @Column
+    @Column(columnDefinition = "varchar(20)")
     private String maker;
 
     // 브랜드번호
     @Column
-    private String brandNo;
+    private long brandNo;
 
     // 과세/비과세
-    @Column
+    @Column(columnDefinition = "varchar(50)")
     private String tax;
 
     // 배송정책
-    @Column
+    @Column(columnDefinition = "varchar(20)")
     private String deliveryType;
 
     // 적립금정책
-    @Column
-    private String useEmoney;
+    @Column(columnDefinition = "varchar(20)")
+    private String useEMoney;
 
     // 유사검색어
-    @Column
+    @Column(columnDefinition = "varchar(100)")
     private String keyword;
 
     // 가격대체문구
-    @Column
+    @Column(columnDefinition = "varchar(50)")
     private String strPrice;
 
     // 짧은설명
-    @Column
+    @Column(columnDefinition = "varchar(30)")
     private String shortDesc;
 
     // 상품설명
-    @Column
+    @Column(columnDefinition = "varchar(100)")
     private String longDesc;
 
     // 메인이미지
-    @Column
+    @Column(columnDefinition = "varchar(100)")
     private String mainImage;
 
     // 리스트이미지
-    @Column
+    @Column(columnDefinition = "varchar(100)")
     private String listImage;
 
     // 상세이미지
-    @Column
+    @Column(columnDefinition = "varchar(100)")
     private String detailImage;
 
     // 확대이미지
-    @Column
+    @Column(columnDefinition = "varchar(100)")
     private String hoverImage;
 
     // 관리메모
-    @Column
+    @Column(columnDefinition = "varchar(100)")
     private String memo;
 
     // 출시일
-    @Column
+    @Column(columnDefinition = "varchar(8)")
     private String launchDaytime;
 
     // 상품표시여부
-    @Column
+    @Column(columnDefinition = "varchar(1)")
     private String openYn;
 
     // 품절여부
-    @Column
+    @Column(columnDefinition = "varchar(1)")
     private String outYn;
 
     // 재고량연동
-    @Column
+    @Column(columnDefinition = "varchar(50)")
     private String useStock;
 
     // 옵션출력방식
-    @Column
+    @Column(columnDefinition = "varchar(20)")
     private String optionType;
 
     // 가격/재고 옵션명
-    @Column
+    @Column(columnDefinition = "varchar(100)")
     private String optionNm;
 
     // 가격/재고 옵션목록
-    @Column
+    @Column(columnDefinition = "varchar(100)")
     private String optionList;
 
     // 추가상품제목
-    @Column
+    @Column(columnDefinition = "varchar(100)")
     private String addOptionNm;
 
     // 추가상품목록
-    @Column
+    @Column(columnDefinition = "varchar(100)")
     private String addOptionList;
 
     // 상품추가정보제목
-    @Column
+    @Column(columnDefinition = "varchar(100)")
     private String exTitle;
 
     // 상품추가정보 1
-    @Column
+    @Column(columnDefinition = "varchar(100)")
     private String ex1;
 
     // 상품추가정보 2
-    @Column
+    @Column(columnDefinition = "varchar(100)")
     private String ex2;
 
     // 상품추가정보 3
-    @Column
+    @Column(columnDefinition = "varchar(100)")
     private String ex3;
 
     // 상품추가정보 4
-    @Column
+    @Column(columnDefinition = "varchar(100)")
     private String ex4;
 
     // 상품추가정보 5
-    @Column
+    @Column(columnDefinition = "varchar(100)")
     private String ex5;
 
     // 상품추가정보 6
-    @Column
+    @Column(columnDefinition = "varchar(100)")
     private String ex6;
 
     // 관련상품목록
-    @Column
+    @Column(columnDefinition = "varchar(100)")
     private String relationList;
 
     // 관련상품번호
-    @Column
+    @Column(columnDefinition = "varchar(20)")
     private String relationNo;
 
     // 타이틀태그
-    @Column
+    @Column(columnDefinition = "varchar(30)")
     private String metaTitle;
 
     // 상품필수정보
-    @Column
+    @Column(columnDefinition = "varchar(100)")
     private String extraInfo;
 
     // 모델명
-    @Column
+    @Column(columnDefinition = "varchar(100)")
     private String modelNm;
 
     // 상품상태
-    @Column
+    @Column(columnDefinition = "varchar(100)")
     private String productStatus;
 
     // 공급가격
-    @Column
+    @Column(columnDefinition = "varchar(100)")
     private String providerPrice;
 
     // 입력옵션제목
-    @Column
+    @Column(columnDefinition = "varchar(100)")
     private String inputAddOptionNm;
 
     // 입력옵션목록
-    @Column
+    @Column(columnDefinition = "varchar(100)")
     private String inputAddOptionList;
 
     // 회원적립금혜택제외
-    @Column
+    @Column(columnDefinition = "varchar(100)")
     private String excludeMemberReserve;
 
     // 회원할인혜택제외
-    @Column
+    @Column(columnDefinition = "varchar(100)")
     private String excludeMemberDiscount;
 
     //제조일자
-    @Column
+    @Column(columnDefinition = "varchar(8)")
     private String manufactureDate;
 
     // 시작유효일자
-    @Column
+    @Column(columnDefinition = "varchar(8)")
     private String effectiveDateStart;
 
     // 종료유효일자
-    @Column
+    @Column(columnDefinition = "varchar(8)")
     private String effectiveDateEnd;
 
     // 이벤트문구
-    @Column
+    @Column(columnDefinition = "varchar(100)")
     private String eventStr;
 }
